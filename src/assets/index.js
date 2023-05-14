39a2da87e651d6459c64ed416c7b9aa20982f41d
Jobayed Sumon <jobayed.sumon@thewickfirm.com>
2023-05-14 21:20:58 +0600

    image optimized

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,6 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -18,11 +17,6 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
 import docker from "./tech/docker.png";
 import vmsl from "./company/vmsl.png";
@@ -70,7 +64,6 @@ export {
   close,
   css,
   flutter,
-  figma,
   git,
   html,
   javascript,
@@ -81,10 +74,6 @@ export {
   tailwind,
   typescript,
   laravel,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   docker,
   vmsl,
   apsis,
